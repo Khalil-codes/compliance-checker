b7d4deab464ec9173cd861bed8df19d2efa56937
Khalil <khalilpati55@gmail.com>
2024-09-19 17:27:43 +0530

    Remove logs and unwanted code

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,21 +1,12 @@
 import { createAdminClient } from "@/lib/supabase/admin";
-import { createClient } from "@/lib/supabase/server";
 
 export const getUsers = async () => {
   const admin = createAdminClient();
-  const supabase = createClient();
 
   const {
     data: { users },
     error,
   } = await admin.listUsers();
-
-  const test = await admin.mfa.listFactors({ userId: users[0].id });
-  const test2 = await supabase.auth.mfa.listFactors();
-
-  console.log(JSON.stringify(test, null, 2));
-  console.log(JSON.stringify(users[0], null, 2));
-  console.log(JSON.stringify(test2, null, 2));
 
   if (error) {
     throw error;
